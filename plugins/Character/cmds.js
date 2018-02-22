5c522e824fda0069ea0ad6638144a37f2f042e47
minimalistic <psix@users.noreply.github.com>
2018-02-22 09:41:37 -0500

    added a couple of concept commands
    * modified >>create
    * added >>reset
    * added >>c
    
    none of these commands should be considered in the final state and are constantly subject to change.

--- plugins/Character/cmds.js
+++ plugins/Character/cmds.js
@@ -2,27 +2,38 @@ const fs=require('fs'); //import the filesystem
 const low=require('lowdb'); //import lowdb which we use for the database
 const fsy=require('lowdb/adapters/FileSync');
 
-exports.commands=["create"] //a list of usable commands
+exports.commands=["create","reset"] //a list of usable commands
 
 exports.create={
   usage:"create",
   description:"Create",
   process:function(bot,msg,suffix){
-    const adapter=new FileSync('test-file.json');
-    const db=low(adapter);
-    if(db.get(`chars.${msg.author.id}`).value()) return; //don't go on if the user already has a character.
-                                                         //TODO: add a cancel message
-    var done=false; //done variable for checking later
-    msg.channel.send(ce(2,`${msg.author.username}, to create your character, react to this message with the class of your choosing.`,null,msg.author))
-      .then(m=>function(){
-        bot.on("messageReactionAdd",reaction,user=>{ //check if a reaction is added to a message
-          const class=reaction._emoji.name.charAt(0).toUpperCase()+reaction._emoji.name.splice(1); //take emoji name then uppercase 1st letter
-          if(!done) {
-            db.set(`chars.${msg.author.id}`, class).write(); //write the player's class to the database
-            done=true;
-            m.edit(ce(1,`${msg.author.username}, you have created your character, which is of the ${class} class.`,null,msg.author));
-          }
-        })
-      })
+    //TODO: sync this with code on the server
+    adapter=new FileSync('path');
+    db=lowdb(adapter);
+    //TODO:stats,inventory,vault
+  }
+}
+
+exports.reset={
+  usage:"reset",
+  description:"Reset",
+  process:function(bot,msg,suffix){
+    if(!fs.existsSync(`users/${msg.author.id}`)) return;
+    fs.unlink(`users/${msg.author.id}`,function(err){if(err) console.log(err)});
+    msg.channel.send('1',`${msg.author.username}, your character was deleted successfully!`,null,msg.author);
+    return;
+  }
+}
+
+exports.c={
+  usage:"c",
+  description:"Char",
+  aliases:["char","character","ch","charinfo"],
+  process:function(bot,msg,suffix){
+    adapter=new FileSync('path');
+    db=lowdb(adapter);
+    var clss=db.get('class').value();
+    return msg.channel.send('2',`${msg.author.username}, your character is a ${clss}.`,null,msg.author);
   }
 }
