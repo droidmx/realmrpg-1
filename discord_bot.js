07192ff04dbefb1a5ad465a9d11291a237d6e34c
Sreyaash <33704504+droidmx@users.noreply.github.com>
2018-05-15 16:46:21 -0400

    Update discord_bot.js

--- discord_bot.js
+++ discord_bot.js
@@ -110,5 +110,5 @@ exports.addCommand = function(commandName, commandObject){
 	
 if(AuthDetails.bot_token){
 	console.log("logging in with token");
-	bot.login(AuthDetails.bot_token);
+	bot.login(process.env.BOT_TOKEN);
 }
